f4d4d357d5817f6bc9889c65bbee0bf387b3efb8
贺鑫鑫 <hexinxin@quyiyuan.com>
2018-03-22 18:40:34 +0800

    描述：更新服务提供者配置文件和controller 作者：mervyn

---
+++ src/main/java/com/mervyn/business/service/impl/AddServiceImpl.java
@@ -0,0 +1,20 @@
+package com.mervyn.business.service.impl;/**
+ * Created by hm on 2018/3/19.
+ */
+
+import com.mervyn.business.service.AddService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author hexinxin
+ * @Date: 2018/3/19
+ * @Time: 14:56
+ * @Description:
+ */
+@Service("addService")
+public class AddServiceImpl implements AddService{
+    @Override
+    public Integer add(Integer augend, Integer addend) {
+        return augend + addend;
+    }
+}
